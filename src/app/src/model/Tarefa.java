855c96e105a427882e260f7b82ef8c84e2e31463
Kawelobo <kawe.lobo@sempreceub.com>
2026-05-23 22:26:10 -0300

    Add Tarefa class with id and nome properties

---
+++ src/app/src/model/Tarefa.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Tarefa {
+
+    private int id;
+    private String nome;
+
+    public Tarefa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
